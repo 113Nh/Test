f370a5de65809cede37332120eacac38a0ef4615
113Nh <cub322y56zai1@outlook.jp>
2023-08-13 23:31:11 +0900

    カリキュラム08-3投稿

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PostController;
+use App\Http\Controllers\PostController; 
 
 /*
 |--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+@@ -13,6 +14,6 @@
 |
 */
+Route::get('/', [PostController::class, 'index']);
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/posts', [PostController::class, 'index']);
+Route::get('/posts/{post}', [PostController::class ,'show']);
+?>
